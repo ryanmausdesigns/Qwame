ae19118315af42eb229d5cc42c6623143b58b1a5
Ryan Maus <rmaus@id.iit.edu>
2018-11-23 16:53:45 -0600

    got the mouseover to work for the main dock! but can't figure out how to make the the new circle stay when arrow is in the correct row

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 // canvas.width = innerWidth;
-canvas.height = 1000;
+canvas.height = 300;
 
 var dockA = new Path2D();
 dockA.arc(170, 30, 20, 0, 2 * Math.PI);
 
+var motherA = new Path2D();
+motherA.rect(0, 0, 200, 50);
+
+var childA1 = new Path2D();
+childA1.arc(120, 30, 20, 0, 2 * Math.PI);
+
 var dockB = new Path2D();
 dockB.arc(170, 80, 20, 0, 2 * Math.PI);
+
+var dockB1 = new Path2D();
+dockB1.arc(120, 80, 20, 0, 2 * Math.PI);
 
 var dockC = new Path2D();
 dockC.arc(170, 130, 20, 0, 2 * Math.PI);
 
+var dockC1 = new Path2D();
+dockC1.arc(120, 130, 20, 0, 2 * Math.PI);
+
 var dockD = new Path2D();
 dockD.arc(170, 180, 20, 0, 2 * Math.PI);
+
+var dockD1 = new Path2D();
+dockD1.arc(120, 180, 20, 0, 2 * Math.PI);
 
 //drawDock 
 ctx.stroke(dockA);
@@ -21,11 +36,12 @@ ctx.stroke(dockB);
 ctx.stroke(dockC);
 ctx.stroke(dockD);
 
+
+
 //mouseOver 
 const statusElement = document.querySelector("#status");
 const dock = []; 
 
-dock.push(dockA, dockB, dockC, dockD)
 
 canvas.addEventListener("mousemove", (event) => {
     let bound = canvas.getBoundingClientRect();
@@ -34,16 +50,17 @@ canvas.addEventListener("mousemove", (event) => {
     let x = event.pageX - bound.top;
     let y = event.pageY - bound.left;
 
-    for (let i = 0; i < dock.length; i++) {
-        const shape = dock[i];
-
-        if (ctx.isPointInPath(shape.path, x, y)) {
-            statusElement.innerText = "Inside Shape";
+    if (ctx.isPointInPath(dockA, x, y)) {
+        if (ctx.isPointInPath(motherA, x, y)) {
+            ctx.stroke(childA1)
+            }else {ctx.clearRect(0, 0, 145, canvas.height)
+            }
+        } else if (ctx.isPointInPath(dockB, x, y)){
+            ctx.stroke(dockB1);
+        } else if (ctx.isPointInPath(dockC, x, y)){
+            ctx.stroke(dockC1);
+        } else if (ctx.isPointInPath(dockD, x, y)){
+            ctx.stroke(dockD1);
         } else {
-            statusElement.innerText = "Outside Shape";
-        }
-
-    }
-});
-
-
+            ctx.clearRect(0, 0, 145, canvas.height);
+        }});
